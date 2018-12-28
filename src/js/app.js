55bf2850dd14e4045eb3e021e51202e0290e78ee
Rom Ogen <romogen2@gmail.com>
2018-12-28 13:15:35 +0200

    started building the raphical from esgraph to see results comfortably

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
 import $ from 'jquery';
-import {parseCode} from './code-analyzer';
+import {parseCode, buildCFG, adaptViz} from './code-analyzer';
+import * as esprima from 'esprima';
+import * as esgraph from 'esgraph';
+import Viz from 'viz.js';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
-        let parsedCode = parseCode(codeToParse);
-        $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        //let parsedCode = parseCode(codeToParse);
+        let dottedFormat = esgraph.dot(buildCFG(codeToParse));
+        let vizAdaptedDot = adaptViz(dottedFormat);
+
+
+        $('#parsedCode').val(JSON.stringify(parsedCode[0], null, 2));
     });
 });
--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,7 +1,36 @@
 import * as esprima from 'esprima';
+import * as esgraph from 'esgraph';
+
+const buildCFG = (codeToParse) => {
+    let cfgJSON = esgraph(esprima.parse(codeToParse, { range: true })['body'][0]['body'])[2];
+    return cfgJSON;
+};
+
+const adaptViz = (dottedFormat) => {
+    return 'digraph CFG { ' + adaptVizVertices(dottedFormat) + adaptVizEdges(dottedFormat) + '}';
+};
+
+const adaptVizEdges = (dottedFormat) => {
+    
+};
+
+const handleVertice = (verticeJSON, verticeNum) => {
+    let verticeAdapted = 'node' + verticeNum + ' [label=' + verticeJSON['codeString'];
+    if(verticeJSON['isGreen'])
+        verticeAdapted += ', color=green, style=filled';
+    return verticeAdapted + ', shape=' + ((verticeJSON['false'] || verticeJSON['true']) ? 'diamond' : 'box') + ']';
+};
+
+const adaptVizVertices = (dottedFormat) => {
+    let adaptedString = '';
+    for (let i = 0 ; i < dottedFormat.length ; i++){
+        adaptedString += handleVertice(dottedFormat[i], i);
+    }
+    return adaptedString;
+};
 
 const parseCode = (codeToParse) => {
     return esprima.parseScript(codeToParse);
 };
 
-export {parseCode};
+export {parseCode, buildCFG, adaptViz};
