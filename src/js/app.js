75496d8e6e981c94eedff036df6eefff64de259e
Rom Ogen <romogen2@gmail.com>
2018-12-31 13:44:13 +0200

    finished printing the graph

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,25 @@ import {parseCode, buildCFG, adaptViz} from './code-analyzer';
 import * as esprima from 'esprima';
 import * as esgraph from 'esgraph';
 import Viz from 'viz.js';
+import { Module, render } from 'viz.js/full.render.js';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         //let parsedCode = parseCode(codeToParse);
-        let dottedFormat = esgraph.dot(buildCFG(codeToParse));
-        let vizAdaptedDot = adaptViz(dottedFormat);
-
+        //let dottedFormat = esgraph.dot(buildCFG(codeToParse));
+        let vizAdaptedDot = adaptViz(buildCFG(codeToParse));
+        renderCfg(vizAdaptedDot);
 
         $('#parsedCode').val(JSON.stringify(parsedCode[0], null, 2));
     });
 });
+
+const renderCfg = (cfg_dot) => {
+    let viz = new Viz({ Module, render });
+    viz.renderSVGElement(cfg_dot)
+        .then(function(element) {
+            document.getElementById('graphPrint').innerHTML = '';
+            document.getElementById('graphPrint').append(element);
+        });
+};
--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -3,28 +3,86 @@ import * as esgraph from 'esgraph';
 
 const buildCFG = (codeToParse) => {
     let cfgJSON = esgraph(esprima.parse(codeToParse, { range: true })['body'][0]['body'])[2];
+    cfgJSON = removeEntry(cfgJSON);
+    cfgJSON = removeExit(cfgJSON);
+
     return cfgJSON;
 };
 
+const getExitNode = (flowNodeArray) => {
+    for (let i = 0 ; i < flowNodeArray.length ; i++){
+        if(flowNodeArray[i]['type'] === 'exit')
+            return i;
+    }
+    return -1;
+};
+
+const removeExitFromNode = (flowNode) => {
+    if(flowNode['normal'] !== undefined && flowNode['normal']['type'] === 'exit'){
+        delete  flowNode['normal'];
+        flowNode['next'] = [];
+        //let exitNextIndex = getExitNode(flowNode['next']);
+        //delete flowNode['next'][exitNextIndex];
+    }
+};
+
+const removeExit = (cfgJSON) => {
+    let exitNodeIndex = getExitNode(cfgJSON);
+    cfgJSON = cfgJSON.slice(0, exitNodeIndex).concat(cfgJSON.slice(exitNodeIndex+1, cfgJSON.length));
+    for (let i = 0 ; i < cfgJSON.length ; i++){
+        removeExitFromNode(cfgJSON[i]);
+    }
+    return cfgJSON;
+};
+
+const removeEntry = (cfgJSON) => {
+    cfgJSON[0].normal.type = 'entry';
+    cfgJSON[0].normal.prev = [];
+    return cfgJSON.slice(1, cfgJSON.length);
+};
+
+
+/******************
+ * dot part start *
+ *****************/
+
+
 const adaptViz = (dottedFormat) => {
-    return 'digraph CFG { ' + adaptVizVertices(dottedFormat) + adaptVizEdges(dottedFormat) + '}';
+    return 'digraph CFG { '  + '\n' + adaptVizVertices(dottedFormat) + '\n' + adaptVizEdges(dottedFormat)  + '\n}';
+};
+
+const addCurrVerticeEdges = (verticeJSON, verticeNum) => {
+    let edgesString = '';
+    if(verticeJSON['true'] !== undefined || verticeJSON['false'] !== undefined){ //'if' node
+        edgesString += 'node' + verticeNum + ' -> node' + verticeJSON['true']['vizNum'] + '[label=T]' + '\n';
+        edgesString += 'node' + verticeNum + ' -> node' + verticeJSON['false']['vizNum'] + '[label=F]' + '\n';
+    }
+    else if(verticeJSON['next'].length !== 0){ // normal
+        edgesString += 'node' + verticeNum + ' -> node' + verticeJSON['normal']['vizNum'] + '\n';
+    }
+    return edgesString;
 };
 
 const adaptVizEdges = (dottedFormat) => {
-    
+    let adaptedEdges = '';
+    for (let i = 0 ; i < dottedFormat.length ; i++){
+        adaptedEdges += addCurrVerticeEdges(dottedFormat[i], i);
+    }
+    return adaptedEdges;
 };
 
 const handleVertice = (verticeJSON, verticeNum) => {
     let verticeAdapted = 'node' + verticeNum + ' [label=' + verticeJSON['codeString'];
     if(verticeJSON['isGreen'])
         verticeAdapted += ', color=green, style=filled';
-    return verticeAdapted + ', shape=' + ((verticeJSON['false'] || verticeJSON['true']) ? 'diamond' : 'box') + ']';
+    return verticeAdapted + ', shape=' + ((verticeJSON['false'] || verticeJSON['true']) ? 'diamond' : 'box') + ']\n';
 };
 
 const adaptVizVertices = (dottedFormat) => {
     let adaptedString = '';
     for (let i = 0 ; i < dottedFormat.length ; i++){
         adaptedString += handleVertice(dottedFormat[i], i);
+        dottedFormat[i]['vizNum'] = i;
     }
     return adaptedString;
 };
